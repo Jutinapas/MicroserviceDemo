623a86f71664e08d5ec6fcf3858a793b03b16bab
Jutinapas Klangcharoenkul <jutinapas@gmail.com>
2018-11-08 14:42:51 +0700

    init project

---
+++ src/main/java/ku/cs/simangkalo/springboot/microservices/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package ku.cs.simangkalo.springboot.microservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
